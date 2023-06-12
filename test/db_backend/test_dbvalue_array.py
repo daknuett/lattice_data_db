df4b75dca653af946941e00bdf443ae3b4176adb
Daniel Knuettel <daniel.knuettel@daknuett.eu>
2023-06-12 18:33:49 +0200

    initial tests passing

--- test/db_backend/test_dbvalue_array.py
+++ test/db_backend/test_dbvalue_array.py
@@ -8,7 +8,7 @@ def test_dbvalue_store_close(tmp_path):
     conn = sqlite3.connect(tmp_path / "test.db", detect_types=sqlite3.PARSE_DECLTYPES)
     schema_init(conn)
     value = DBValue(np.arange(-1, 2, 0.01))
-    rid = value1.store(conn)
+    rid = value.store(conn)
 
     value2 = DBValue.load(conn, rid)
 
