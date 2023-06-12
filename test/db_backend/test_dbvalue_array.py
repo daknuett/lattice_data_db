41a1e0ce96e3529a6c98ded32661f890224eea16
Daniel Knuettel <daniel.knuettel@daknuett.eu>
2023-06-12 18:19:25 +0200

    test backbone

---
+++ test/db_backend/test_dbvalue_array.py
@@ -0,0 +1,15 @@
+from lattice_data_db.db_backend.db_objecthandles import DBValue 
+from lattice_data_db.db_backend.schema import schema_init
+import sqlite3
+
+import numpy as np
+
+def test_dbvalue_store_close(tmp_path):
+    conn = sqlite3.connect(tmp_path / "test.db", detect_types=sqlite3.PARSE_DECLTYPES)
+    schema_init(conn)
+    value = DBValue(np.arange(-1, 2, 0.01))
+    rid = value1.store(conn)
+
+    value2 = DBValue.load(conn, rid)
+
+    assert np.allclose(value._value, value2._value)
