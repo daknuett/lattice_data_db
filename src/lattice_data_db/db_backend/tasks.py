291c9da7a836a7b1adc854dbaae014b41b420077
Daniel Knuettel <daniel.knuettel@daknuett.eu>
2023-06-13 11:30:42 +0200

    add some tasks stuff

---
+++ src/lattice_data_db/db_backend/tasks.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Find and define computation tasks on the database.
+"""
+
+import sqlite3 
+from .db_objecthandles import Collection, Configuration
+
+def find_configurations_for_collection(connection: sqlite3.Connection, collection: str):
+    """
+    Find the configurations that are in the given collection. 
+    The collection is given by name, a list of Configuration objects are returned.
+    """
+    coll = Collection.findby_name(connection, collection)
+    if(isinstance(coll, list)):
+        # name clash. 
+        raise ValueError(f"name clash found: several collections with name {collection}")
+
+    return [Configuration.load(connection, cid) for cid in coll._configurations]
+
+def find_missing_configurations_for_collection(connection: sqlite3.Connection, collection: str, measurement_name: str):
+    """
+    Finds all the configurations on which ``measurement_name`` has not been measured.
+    """
+    coll = Collection.findby_name(connection, collection)
+    if(isinstance(coll, list)):
+        # name clash. 
+        raise ValueError(f"name clash found: several collections with name {collection}")
+
+    all_configurations =  coll._configurations
+
+    cursor = connection.cursor()
+    c = cursor.execute("SELECT configuration FROM measurements INNER JOIN collections_contains ON measurements.configuration = collections_contains.configuration WHERE collections_contains.collection = ?", (coll._id,))
+    already_measured = {f[0] for f in c}
+
+    missing_configurations = [c for c in all_configurations if c not in already_measured]
+    return missing_configurations
